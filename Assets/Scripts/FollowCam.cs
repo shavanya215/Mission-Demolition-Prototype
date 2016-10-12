b7ede8045cab4929f2d725f1b7ffccab19b5fc5a
shavanya215 <shavanya.215@gmail.com>
2016-10-12 14:26:00 -0400

    Camera, Slingshot and physics, cloud

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour
+{
+    static public FollowCam S; // a FollowCam Singleton 
+    //fields are set in the unity Inspector pane 
+    public float      easing = 0.05f;
+    public Vector2    minXY;
+    public bool       _______________________________;
+
+    // fields set dynamically 
+    public GameObject poi; //the point of interest 
+    public float camZ;// The desired Z pos of the camera 
+
+
+    void Awake()
+    {
+        S = this;
+        camZ = this.transform.position.z;
+    }
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        //if there's only one line following an if, it doesn't need braces 
+        if (poi == null) return; // return if there is no poi
+
+        //Get the position of the poi 
+        Vector3 destination = poi.transform.position;
+        //Limit the X & Y to the minimum value 
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        //Interpolate from the current Camera Position toward desination
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        //Retain a destination.z of CamZ
+        destination.z = camZ;
+        //Set the camera to the destination 
+        transform.position = destination;
+        //Set the orthographicSize of the Camera to keep Ground in View 
+        this.GetComponent<Camera>().orthographicSize = destination.y + 10;
+
+    }
+}
--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Slingshot : MonoBehaviour {
     public GameObject    prefabProjectile;
+    public float         velocityMult = 4f;
     public bool          _________________________________; 
    
     // fields set dynamically 
@@ -39,8 +40,41 @@ public class Slingshot : MonoBehaviour {
         //set it to iaKinematic for now 
         projectile.GetComponent<Rigidbody>().isKinematic = true;
     }
+
 	// Update is called once per frame
 	void Update () {
-	
+        // if Slingshot is not in aimingMode, don't run this cose 
+        if (!aimingMode) return;
+        //Get the current Mouse position in 2d screen coordinates 
+        Vector3 mousePos2D = Input.mousePosition;
+        // Convert the mouse position to 3D world coordinates 
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        //find the delta from the launchpos to the mousepos3d
+        Vector3 mouseDelta = mousePos3D-launchPos;
+        //Limit mouseDelta to the radius of the slingshot sphereCollider; 
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+        //Move the projectile to this new position 
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+
+        if ( Input.GetMouseButtonUp(0) ) {
+            // The mouse has been released 
+            aimingMode = false;
+            projectile.GetComponent<Rigidbody>().isKinematic = false;
+            projectile.GetComponent<Rigidbody>().velocity = -mouseDelta * velocityMult;
+            FollowCam.S.poi = projectile;
+            projectile = null;
+
+        }
+
+
+        }
+
 	}
-}
+
