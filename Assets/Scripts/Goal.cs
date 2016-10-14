0b0d6f04aee26b7d1d393b06e889e6f043395225
shavanya215 <shavanya.215@gmail.com>
2016-10-13 21:54:11 -0400

    goal

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -2,9 +2,25 @@
 using System.Collections;
 
 public class Goal : MonoBehaviour {
+    // A static field accessible by code anywhere 
+    static public bool        goalMet = false; 
+
 
 	// Use this for initialization
-	void Start () {
+	void OnTriggerEnter( Collider other) {
+        //When the trigger is hit by something
+        //cheeck to see if it's a projectile 
+        if ( other.gameObject.tag == "Projectile") {
+            //if so, set goalMet to true 
+            Goal.goalMet = true;
+
+            // Also set the alpha of the color to higher opactiy 
+            Color c = GetComponent<Renderer>().material.color;
+            c.a = 0.9f;
+            GetComponent<Renderer>().material.color = c;
+
+
+        }
 	
 	}
 	
